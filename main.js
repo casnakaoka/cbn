c1b07092ba6c21c1b9c9f341f3b7199c6da6a760
casnakaoka <61786661+casnakaoka@users.noreply.github.com>
2020-05-14 20:57:38 -0300

    e-commerce home
    try to represent  a e-commerce home page, with itens photos and options to change exchange e etc

---
+++ main.js
@@ -0,0 +1,17 @@
+/*FIRST SLIDER */
+$(`.slider-one`).not(".slick-intialized").slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+  prevArrow: ".site-slider .slider-btn .prev",
+  nextArrow: ".site-slider .slider-btn .next",
+});
+
+/*SECOND SLIDER */
+$(`.slider-two`).not(".slick-intialized").slick({
+  prevArrow: ".site-slider-two .prev",
+  nextArrow: ".site-slider-two .next",
+  slidesToShow: 5,
+  slidesToScrow: 1,
+  autoplaySpeed: 3000,
+});
